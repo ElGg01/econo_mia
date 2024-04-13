a3471db847d024220ddd0f939392a60a0572dc94
IvanGallegosAragon <ivangallegos13@aragon.unam.mx>
2024-04-13 17:17:27 -0600

    new scheme for user and transactions to DB, query functions for elements of the month and total sum in points

--- lib/functionsBF/serviciosFireBase.dart
+++ lib/functionsBF/serviciosFireBase.dart
@@ -10,253 +10,119 @@ Future<void> addUser(String user) async {
   //de la session actual
   //Inicialización de subcolección
   //Es una inicialización de campos vacios solo para que se genere el usuario sin campos
-  final datos = <String, dynamic>{
+  final datosTransactions = <String, dynamic>{
     "monto": 0,
     "fecha": DateTime.now(),
+    "concepto": "newUser",
+    "tipo": "none", //Ingreso o Egreso
   };
-  //Subcoleccion de ingresos
+
+  final datosAssum = <String, dynamic>{
+    "expense": 0,
+    "name": "",
+  };
+  //Subcoleccion de transacciones
   //!!!!!!!!!!!!!!CAMBIAR EL USUARIO AL ACTIVO
-  final refIngresos =
-      db.collection('users').doc(user).collection('ingresos').doc();
-  //Generación de subcoleccion de ingresos
-  refIngresos.set(datos);
-  //Subcoleccion de egresos
-  //!!!!!!!!!!!!!!CAMBIAR EL USUARIO AL ACTIVO
-  final refEgresos =
-      db.collection('users').doc(user).collection('egresos').doc();
-  //Generación de subcoleccion de ingresos
-  refEgresos.set(datos);
+  final refTransactions =
+      db.collection('users').doc(user).collection('transactions').doc();
+
+  refTransactions.set(datosTransactions);
+
+  //Subcoleccion de Supuestos
+  final refSupues =
+      db.collection('users').doc(user).collection('assumption').doc();
+  refSupues.set(datosAssum);
 
   //Generación de campo SALDO, este cambiara con cada ingreso y egreso
   final refUsuario = db.collection('users').doc(user);
   refUsuario.set({"saldo": 0});
 }
 
-//------------------------Agregar Ingreso-----------------------------------
-Future<void> addIngreso(double monto, String user) async {
-  //Agrega un ingreso conforme al monto y el identificador en String del usuario actual
-  //así como atualizar el cambo saldo del usuario
-  final transaccion = {
-    "monto": monto,
+//------------------------Agregar Transaccion-----------------------------------
+Future<void> addTransaction(
+    String user, double monto, String concepto, int tipo) async {
+  ///Agrega una transaccion conforme al monto (ingresarlo en valor absoluto), sera positivo
+  ///negativo conforme al tipo ya sea 1 (+monto) o 0 (-monto)
+  ///el identificador en String del usuario actual
+  ///ingresar concepto.
+  final transaccion = <String, dynamic>{
+    "monto": tipo == 1 ? monto : -monto,
     "fecha": DateTime.now(),
+    "concepto": concepto,
+    "tipo": tipo,
   };
   //Demo es el usuario cambialo por el user de autentication
-  db.collection("users").doc(user).collection('ingresos').add(transaccion);
-  //Actualizamos el saldo
-  db.collection("users").doc(user).update(
-    {"saldo": FieldValue.increment(monto)},
-  );
-}
-//------------------------Agregar Egreso-----------------------------------
+  db.collection("users").doc(user).collection('transactions').add(transaccion);
 
-Future<void> addEgreso(double monto, String user) async {
-  final transaccion = {
-    "monto": monto,
-    "fecha": DateTime.now(),
-  };
-  //Demo es el usuario cambialo por el user de autentication
-  db.collection("users").doc(user).collection('egresos').add(transaccion);
   //Actualizamos el saldo
-  //Parceamos
   db.collection("users").doc(user).update(
-    {"saldo": FieldValue.increment(-monto)},
+    {"saldo": FieldValue.increment(transaccion["monto"])},
   );
 }
 
 //------------------------Consultas-----------------------------------
-//Consulta ingresos desde siempre
-//Utiliza el identificador del usuario como String
-//Lista que contiene elementos  de todos los ingresos realizados por el usuario
-//La Date esta como un DateTime así que al momento de usarlo cortalo a conveniencia
-Future<List> consultaIngresosSiempre(String user) async {
+//Consulta de todos los elementos del mes
+//Devuelve una lista que contiene los elementos del mes a consultar conforme al mes y el año
+//Cada elemento es una tupla del tipo (monto,conpecto,tipo,dia_del_mes)
+Future<List> consultaElementosMes(String user, int year, int month) async {
+  ///Devulve lista (monto,conpecto,tipo,dia_del_mes)
+
   //Lista de importancia
-  List ingresosSiempre = [];
+  List elementosDelMes = [];
+  //Fecha
+  DateTime fechaDown = DateTime(year = year, month = month);
+  DateTime fechaUp = DateTime(year = year, month = month + 1);
+
   //Refrencia a la colección
   CollectionReference collectionReference =
-      db.collection('users').doc(user).collection("ingresos");
+      db.collection('users').doc(user).collection("transactions");
   //consulta
-  QuerySnapshot queryIngresos = await collectionReference.get();
-
-  for (var document in queryIngresos.docs) {
-    ingresosSiempre.add(document.data());
-  }
-  return ingresosSiempre;
-}
-
-//Consulta egresos desde siempre
-//Lista que contiene elementos  de todos los egresos realizados por el usuario
-//La Date esta como un DateTime así que al momento de usarlo cortalo a conveniencia
-Future<List> consultaEgresosSiempre(String user) async {
-  //Lista de importancia
-  List egresosSiempre = [];
-  //Refrencia a la colección
-  CollectionReference collectionReference =
-      db.collection('users').doc(user).collection("egresos");
-  //consulta
-  QuerySnapshot queryIngresos = await collectionReference.get();
-
-  for (var document in queryIngresos.docs) {
-    egresosSiempre.add(document.data());
-  }
-  return egresosSiempre;
-}
-
-//Consulta de Ingresos totales del usuario en todo el tiempo
-//Es una lista devuelve la suma de todos los ingresos exitentes en
-//la subcoleccion de ingresos
-//Si puedes cambiarlo a un solo double, mejor.
-Future<List> consultaIngresosTotales(String user) async {
-  List monto = [];
-  //Cambia el usuario demo por el usuario actual
-  CollectionReference collectionReference =
-      db.collection('users').doc(user).collection("ingresos");
-
-  AggregateQuerySnapshot aggregateQuerySnapshotSum =
-      await collectionReference.aggregate(sum('monto')).get();
-  monto.add(aggregateQuerySnapshotSum.getSum('monto'));
-  return monto;
-}
-
-//Consulta de Ingresos totales del usuario en todo el tiempo
-//Es una lista devuelve la suma de todos los ingresos exitentes en
-//la subcoleccion de ingresos
-//Si puedes cambiarlo a un solo double, mejor.
-Future<List> consultaEgresosTotales(String user) async {
-  List monto = [];
-  //Cambia el usuario demo por el usuario actual
-  CollectionReference collectionReference =
-      db.collection('users').doc(user).collection("egresos");
-
-  AggregateQuerySnapshot aggregateQuerySnapshotSum =
-      await collectionReference.aggregate(sum('monto')).get();
-  monto.add(aggregateQuerySnapshotSum.getSum('monto'));
-  return monto;
-}
-
-//Consulta Ingresos del dia
-//Lista que contiene elementos  de todos los ingresos realizados por el usuario
-//La Date esta como un DateTime así que al momento de usarlo cortalo a conveniencia
-Future<List> consultaIngresosDelDia(String user) async {
-  //Lista de importancia
-  List ingresosDelDia = [];
-  DateTime dia =
-      DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day);
-  //Refrencia a la colección
-  CollectionReference collectionReference =
-      db.collection('users').doc(user).collection("ingresos");
-  //consulta
-  QuerySnapshot queryIngresos = await collectionReference
-      .where('fecha', isGreaterThanOrEqualTo: dia)
+  QuerySnapshot queryElementos = await collectionReference
+      .where("fecha", isGreaterThanOrEqualTo: fechaDown)
+      .where("fecha", isLessThan: fechaUp)
       .get();
 
-  for (var document in queryIngresos.docs) {
-    ingresosDelDia.add(document.data());
+  for (var document in queryElementos.docs) {
+    elementosDelMes.add((
+      document.get(FieldPath(const ['monto'])),
+      document.get(FieldPath(const ['concepto'])),
+      document.get(FieldPath(const ['tipo'])) == 1 ? "ingreso" : 'egreso',
+      document.get(FieldPath(const ['fecha'])).toDate().day
+    ));
   }
-  return ingresosDelDia;
+  return elementosDelMes;
 }
 
-//Consulta Egresos del dia
-//Lista que contiene elementos  de todos los egresos realizados por el usuario
-//La Date esta como un DateTime así que al momento de usarlo cortalo a conveniencia
-Future<List> consultaEgresosDelDia(String user) async {
+Future<List> consultaP(String user, int year, int month) async {
+  ///Devulve lista (monto,conpecto,tipo,dia_del_mes)
+
+  //Mapa de elementos
+  Map<int, num> mapa = {};
   //Lista de importancia
-  List egresosDelDia = [];
-  DateTime dia =
-      DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day);
+  List elementosDelMes = [];
+  //Fecha
+  DateTime fechaDown = DateTime(year = year, month = month);
+  DateTime fechaUp = DateTime(year = year, month = month + 1);
+
   //Refrencia a la colección
   CollectionReference collectionReference =
-      db.collection('users').doc(user).collection("egresos");
+      db.collection('users').doc(user).collection("transactions");
   //consulta
-  QuerySnapshot queryIngresos = await collectionReference
-      .where('fecha', isGreaterThanOrEqualTo: dia)
+  QuerySnapshot queryElementos = await collectionReference
+      .where("fecha", isGreaterThanOrEqualTo: fechaDown)
+      .where("fecha", isLessThan: fechaUp)
       .get();
 
-  for (var document in queryIngresos.docs) {
-    egresosDelDia.add(document.data());
+  //Generamos toda la lista de puntos generales
+  for (var document in queryElementos.docs) {
+    var key = document.get(FieldPath(const ['fecha'])).toDate().day;
+    var value = document.get(FieldPath(const ['monto']));
+    if (mapa.containsKey(key)) {
+      value = mapa[key]! + value;
+    }
+    mapa.addAll({key: value});
   }
-  return egresosDelDia;
-}
 
-//Consulta Egresos del mes
-//Lista que contiene elementos  de todos los egresos realizados por el usuario durante el mes
-//La Date esta como un DateTime así que al momento de usarlo cortalo a conveniencia
-Future<List> consultaEgresosDelMes(String user) async {
-  //Lista de importancia
-  List egresosDelMes = [];
-  DateTime mes = DateTime(DateTime.now().year, DateTime.now().month);
-  //Refrencia a la colección
-  CollectionReference collectionReference =
-      db.collection('users').doc(user).collection("egresos");
-  //consulta
-  QuerySnapshot queryIngresos = await collectionReference
-      .where('fecha', isGreaterThanOrEqualTo: mes)
-      .get();
-
-  for (var document in queryIngresos.docs) {
-    egresosDelMes.add(document.data());
-  }
-  return egresosDelMes;
-}
-
-//Consulta Ingresos del mes
-//Lista que contiene elementos  de todos los egresos realizados por el usuario durante el mes
-//La Date esta como un DateTime así que al momento de usarlo cortalo a conveniencia
-Future<List> consultaIngresosDelMes(String user) async {
-  //Lista de importancia
-  List ingresosDelMes = [];
-  DateTime mes = DateTime(DateTime.now().year, DateTime.now().month);
-  //Refrencia a la colección
-  CollectionReference collectionReference =
-      db.collection('users').doc(user).collection("ingresos");
-  //consulta
-  QuerySnapshot queryIngresos = await collectionReference
-      .where('fecha', isGreaterThanOrEqualTo: mes)
-      .get();
-
-  for (var document in queryIngresos.docs) {
-    ingresosDelMes.add(document.data());
-  }
-  return ingresosDelMes;
-}
-
-//Consulta Ingresos del año
-//Lista que contiene elementos  de todos los egresos realizados por el usuario durante el año
-//La Date esta como un DateTime así que al momento de usarlo cortalo a conveniencia
-Future<List> consultaIngresosDelyear(String user) async {
-  //Lista de importancia
-  List ingresosDelMes = [];
-  DateTime ano = DateTime(DateTime.now().year);
-  //Refrencia a la colección
-  CollectionReference collectionReference =
-      db.collection('users').doc(user).collection("ingresos");
-  //consulta
-  QuerySnapshot queryIngresos = await collectionReference
-      .where('fecha', isGreaterThanOrEqualTo: ano)
-      .get();
-
-  for (var document in queryIngresos.docs) {
-    ingresosDelMes.add(document.data());
-  }
-  return ingresosDelMes;
-}
-
-//Consulta Ingresos del año
-//Lista que contiene elementos  de todos los egresos realizados por el usuario durante el año
-//La Date esta como un DateTime así que al momento de usarlo cortalo a conveniencia
-Future<List> consultaEgresosDelyear(String user) async {
-  //Lista de importancia
-  List egresosDelMes = [];
-  DateTime ano = DateTime(DateTime.now().year);
-  //Refrencia a la colección
-  CollectionReference collectionReference =
-      db.collection('users').doc(user).collection("egresos");
-  //consulta
-  QuerySnapshot queryIngresos = await collectionReference
-      .where('fecha', isGreaterThanOrEqualTo: ano)
-      .get();
-
-  for (var document in queryIngresos.docs) {
-    egresosDelMes.add(document.data());
-  }
-  return egresosDelMes;
+  elementosDelMes = [for (var e in mapa.entries) (e.key, e.value)];
+  return elementosDelMes;
 }
